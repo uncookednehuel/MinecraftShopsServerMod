52283c1d4775cb07063ef68ac1b961fb65b5affc
uncoolnehuel <unconfigured@null.spigotmc.org>
2021-05-14 14:40:44 -0400

    Added Kevlar Recipe, Kevlar Armor

---
+++ src/main/java/net/mcreator/minecraftshopsmod/item/KevlarItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.minecraftshopsmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.minecraftshopsmod.itemgroup.MinecraftShopsModTabItemGroup;
+import net.mcreator.minecraftshopsmod.MinecraftShopsModModElements;
+
+@MinecraftShopsModModElements.ModElement.Tag
+public class KevlarItem extends MinecraftShopsModModElements.ModElement {
+	@ObjectHolder("minecraft_shops_mod:kevlar")
+	public static final Item block = null;
+	public KevlarItem(MinecraftShopsModModElements instance) {
+		super(instance, 15);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(MinecraftShopsModTabItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("kevlar");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
